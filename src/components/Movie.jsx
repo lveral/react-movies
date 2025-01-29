fd05daeb486f765805bed9b9ad3d94a537b5cbdd
Vera <63463936+lveral@users.noreply.github.com>
2025-01-29 23:23:32 +0500

    complete

---
+++ src/components/Movie.jsx
@@ -0,0 +1,26 @@
+function Movie (props) {
+    const {
+        Poster: poster,
+        Title: title,
+        Type: type,
+        Year: year,
+        imdbID: id
+    } = props;
+    return <div id={id} className="movie card">
+        <div className="card-image waves-effect waves-block waves-light">
+            {
+                (poster === "N/A") ?
+                    <img className="activator" src={`https://place-hold.it/300x440&text=Not%20found`} alt="Poster"/>
+                    :
+                    <img className="activator" src={poster} alt="Poster"/>
+            }
+        </div>
+        <div className="card-content">
+            <span className="card-title activator grey-text text-darken-4">{title}</span>
+                <p>{year} <span className="right">{type}</span></p>
+
+        </div>
+    </div>
+}
+
+export {Movie}

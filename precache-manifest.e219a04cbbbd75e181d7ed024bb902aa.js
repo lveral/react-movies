ff930008a8da11d3631f151878a922f3d1c1aaf7
Vera <63463936+lveral@users.noreply.github.com>
2025-01-30 13:13:44 +0500

    Updates

---
+++ precache-manifest.e219a04cbbbd75e181d7ed024bb902aa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "117afdc62471c323229b12904f5011f0",
+    "url": "/react-movies/index.html"
+  },
+  {
+    "revision": "5c1acd5e6051e08af17f",
+    "url": "/react-movies/static/css/main.fc2b203f.chunk.css"
+  },
+  {
+    "revision": "638e200e69d950262938",
+    "url": "/react-movies/static/js/2.ae026ab9.chunk.js"
+  },
+  {
+    "revision": "5c1acd5e6051e08af17f",
+    "url": "/react-movies/static/js/main.ec79ad9c.chunk.js"
+  },
+  {
+    "revision": "cdb8474b72bd7dff1c63",
+    "url": "/react-movies/static/js/runtime~main.bf100f6d.js"
+  }
+]);
